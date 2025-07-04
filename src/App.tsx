f5d130eeb02fb29a76b5b4a18dcecd048eab8ea9
Mngoc23 <nguyengoc2323@gmail.com>
2025-07-05 00:44:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
 
-// Wrapper để dùng key=id bắt React remount lại ProductPage khi id thay đổi
 function ProductPageWrapper() {
   const { id } = useParams();
   return <ProductPage key={id} />;
